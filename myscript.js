73ee0b8949176b08b829b37ed294761c489f0bab
rrose003 <rachelsmacair@rachels-air.lan>
2022-07-06 16:11:37 -0600

    Add play function and reset function

--- myscript.js
+++ myscript.js
@@ -6,7 +6,69 @@ const play = function(playerChoice) {
     document.getElementById('opponent').innerHTML = '';
     document.getElementById('results').innerHTML = '';
 
+    if (playerChoice === 'rock' || playerChoice === 'paper' || playerChoice === 'scissors') {
+        document.getElementById('player').innerHTML = `You chose ${playerChoice}. `;
+    } else {
+        document.getElementById('player').innerHTML = 'That is not a valid choice, try again.';
+        return false;
+    }
+
+    var computerChoice = Math.random();
+    if (computerChoice < 0.34) {
+        computerChoice = 'rock';
+    } else if (computerChoice <= 0.67 && computerChoice > 0.33) {
+        computerChoice = 'paper';
+    } else {
+        computerChoice = 'scissors';
+    }
+    document.getElementById('opponent').innerHTML = `// Your opponent chose ${computerChoice}.`;
     
-}
+    var compare = function(choice1,choice2) {
+        if (choice1 === choice2) {
+            return 'The result is a tie!';
+        } else if (choice1 === 'rock') {
+            if (choice2 === 'scissors') {
+                wins++;
+                return 'Rock wins!';
+            } else {
+                losses++;
+                return 'Sorry, paper wins!';
+            }
+        } else if (choice1 === 'paper') {
+            if (choice2 === 'rock') {
+                wins++;
+                return 'Paper wins!';
+            } else {
+                losses++;
+                return 'Sorry, scissors wins!';
+            }
+        } else if (choice1 === 'scissors') {
+            if (choice2 === 'rock') {
+                losses++;
+                return 'Sorry, rock wins!';
+            } else {
+                wins++;
+                return 'Scissors win!';
+            }
+        } else {
+            return 'Game over.';
+        }
+    }
+    var winner = compare(playerChoice, computerChoice);
+    document.getElementById('results').innerHTML = winner;
+    document.getElementById('wins').innerHTML = wins;
+    document.getElementById('losses').innerHTML = losses;
+};
+
+var reset = function() {
+    losses = 0;
+    wins = 0;
+    document.getElementById('wins').innerHTML = wins;
+    document.getElementById('losses').innerHTML = losses;
+    document.getElementById('results').innerHTML = '';
+    document.getElementById('player').innerHTML = '';
+    document.getElementById('opponent').innerHTML = '';
+};
+
 
 
